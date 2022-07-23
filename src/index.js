b2365838861c16d14319d20b5183258e8120cb61
van-matos <vanmatos@poli.ufrj.br>
2022-07-23 15:49:45 -0300

    feat: add routes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
+import categoriesRouter from "./routes/categoriesRouter.js";
+import customersRouter from "./routes/customersRouter.js";
+import gamesRouter from "./routes/gamesRouter.js";
+import rentalsRouter from "./routes/rentalsRouter.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(categoriesRouter);
+app.use(customersRouter);
+app.use(gamesRouter);
+app.use(rentalsRouter);
 
 const PORT = process.env.PORT || 4000;
 
