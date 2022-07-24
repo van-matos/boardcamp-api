072beeedd105860ad885a58a79fe1c685981a0db
van-matos <vanmatos@poli.ufrj.br>
2022-07-24 20:27:26 -0300

    feat: add GET for '/rentals'

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -18,3 +18,75 @@ export async function createRentals (req, res) {
         res.sendStatus(500);
     }
 }
+
+export async function readRentals (req, res) {
+    const { customerId, gameId } = req.query;
+    let queryParams = "";
+
+    try {
+        if (customerId) {
+            queryParams = `WHERE rentals."customerId" = ${customerId}`
+        };
+
+        if (gameId) {
+            queryParams = `WHERE rentals."gameId" = ${gameId}`
+        };
+
+        const { rows: dbRentals } = await connection.query(`
+        SELECT rentals.*,
+            games.id as "gameId",
+            games.name as "gameName",
+            categories.id as "categoryId",
+            categories.name as "categoryName",
+            customers.id as "customerId",
+            customers.name as "customerName" 
+        FROM rentals 
+        JOIN customers ON rentals."customerId" = customers.id 
+        JOIN games ON rentals."gameId" = games.id 
+        JOIN categories ON games."categoryId" = categories.id 
+        ${queryParams}
+        `);
+
+        const rentals = formatRentals(dbRentals);
+
+        return res.send(rentals);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+function formatRentals (dbRentals) {
+    const rentals = []
+
+    for (let rental of dbRentals) {
+        const returnDate = rental.returnDate ? dayjs(rental.returnDate).format("YYYY-MM-DD") : null;
+
+        rental = {
+            ...rental,
+            rentDate: dayjs(rental.rentDate).format("YYYY-MM-DD"),
+            returnDate: returnDate,
+            customer: {
+                id: rental.customerId,
+                name: rental.customerName,
+            },
+            game: {
+                id: rental.gameId,
+                name: rental.gameName,
+                categoryId: rental.categoryId,
+                categoryName: rental.categoryName,
+            },
+        };
+
+        delete rental.customerId;
+        delete rental.customerName;
+        delete rental.gameId;
+        delete rental.gameName;
+        delete rental.categoryId;
+        delete rental.categoryName;
+        
+        rentals.push(rental);
+    }
+
+    return rentals;
+}
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { createRentals } from "../controllers/rentalsController.js";
+import { createRentals, readRentals } from "../controllers/rentalsController.js";
 import { validateRental } from "../middlewares/rentalsMiddleware.js";
 
 const router = Router();
 
+router.get("/rentals", readRentals);
 router.post("/rentals", validateRental, createRentals);
 
 export default router;
