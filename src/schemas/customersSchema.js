33cc6694f2f93f42f0179cd90e65c057f3966dbc
van-matos <vanmatos@poli.ufrj.br>
2022-07-25 18:53:29 -0300

    fix: 'customerSchema' checks if submitted birthday not in future

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -2,9 +2,9 @@ import joi from "joi";
 
 const customersSchema = joi.object({
     name: joi.string().required(),
-    cpf: joi.string().regex(/[0-9]{11}/),
-    phone: joi.string().regex(/[0-9]{10,11}/),
-    birthday: joi.date().iso().required()
+    cpf: joi.string().length(11).required(),
+    phone: joi.string().min(10).max(11).required(),
+    birthday: joi.date().max(Date.now()).required()
 });
 
 export default customersSchema;
